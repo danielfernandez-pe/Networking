d7c9ec632025a1bfa7a9700533021cc80b7f8ba8
Daniel F <daniel.yopla@team.wrike.com>
2024-06-19 07:51:39 +0200

    Adding query gets

--- Sources/FirebaseNetworking.swift
+++ Sources/FirebaseNetworking.swift
@@ -15,7 +15,28 @@ public struct FirebaseNetworking {
 
     public func get(path: String) async throws -> Data {
         let snapshot = try await db.collection(path).getDocuments()
-        let dictArray = snapshot.documents.map { $0.data() }
+        let dictArray = snapshot.documents.map {
+            var dict = $0.data()
+            dict["id"] = $0.documentID
+            return dict
+        }
+        return try JSONSerialization.data(withJSONObject: dictArray, options: [])
+    }
+
+    public func getDocument(path: String, id: String) async throws -> Data {
+        let snapshot = try await db.collection(path).document(id).getDocument()
+        var dict = snapshot.data() ?? [:]
+        dict["id"] = snapshot.documentID
+        return try JSONSerialization.data(withJSONObject: dict, options: [])
+    }
+
+    public func get(path: String, queryField: String, value: Any) async throws -> Data {
+        let snapshot = try await db.collection(path).whereField(queryField, isGreaterThanOrEqualTo: value).getDocuments()
+        let dictArray = snapshot.documents.map {
+            var dict = $0.data()
+            dict["id"] = $0.documentID
+            return dict
+        }
         return try JSONSerialization.data(withJSONObject: dictArray, options: [])
     }
 }
